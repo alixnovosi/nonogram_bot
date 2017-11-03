a583e6db63442bdf086ee07b8d1007ee5d16434a
Andrew Michaud <dev@mail.andrewmichaud.com>
2017-11-03 01:51:56 -0700

    oops way past initial commit

---
+++ nonobot/__main__.py
@@ -0,0 +1,50 @@
+"""Main class for bot."""
+
+import random
+import sys
+import time
+from datetime import datetime
+from os import path
+
+import botskeleton
+import nonogen
+
+
+# Delay between tweets in seconds.
+DELAY = 3600 * 6
+
+
+if __name__ == "__main__":
+    SECRETS_DIR = path.join(path.abspath(path.dirname(__file__)), "SECRETS")
+    BOT_SKELETON = botskeleton.BotSkeleton(SECRETS_DIR, bot_name="nono_bot", delay=DELAY)
+
+    LOG = botskeleton.set_up_logging()
+
+    FILENAME = f"nonogrid_{datetime.now()}.jpg"
+
+    while True:
+        LOG.info("Generating a puzzle")
+        grid = nonogen.nono.NonoGrid(random.choice(range(5, 21)), random.choice(range(5, 21)))
+
+        LOG.info(f"Generated a {len(grid.squares[0])} by {len(grid.squares)} puzzle.")
+
+        LOG.info("Putting stuff in the puzzle.")
+        nonogen.gen_perlin(grid)
+
+        LOG.info("Generating hints for the stuff.")
+        grid.gen_hints()
+
+        LOG.info("Printing the hints and stuff.")
+        grid.to_picture(FILENAME)
+
+        LOG.info("Saving with solutions as well.")
+        grid.to_picture(f"solved_{FILENAME}")
+
+        encoded = grid.encode()
+        print(f"Puzzle code is: {encoded}")
+
+        LOG.info("Sending the picture, and hints and stuff.")
+        LOG.info(f"The puzzle, without filled squares, is \n{grid}")
+        status = BOT_SKELETON.send_with_one_media(f"Here's a puzzle! Encoded as: {encoded}", FILENAME)
+
+        BOT_SKELETON.nap()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from os import path
+from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, "VERSION"), encoding="utf-8") as f:
+    VERSION = f.read().strip()
+
+setup(author="Andrew Michaud",
+      author_email="bots@mail.andrewmichaud.com",
+
+      entry_points={
+          "console_scripts": ["nonobot_bot = nonobot_bot.__main__:main"]
+      },
+
+      install_requires=["botskeleton>=1.0.4", "nonogen"],
+
+      license="BSD3",
+
+      name="nonobot_bot",
+
+      packages=find_packages(),
+
+      # Project"s main homepage
+      url="https://github.com/andrewmichaud/nono_bot",
+
+      version=VERSION)
