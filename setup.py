a8a12a03d81a88823dd62118a6c3a10d7ba77a52
Andrew Michaud <dev@mail.andrewmichaud.com>
2018-08-29 13:50:49 -0700

    pin to new multi-output botskeleton

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["nonogram_bot = nonogram_bot.__main__:main"]
       },
 
-      install_requires=["botskeleton>=2.2.0", "nonogen"],
+      install_requires=["botskeleton>=3.0.1", "nonogen"],
 
       license="BSD3",
 
